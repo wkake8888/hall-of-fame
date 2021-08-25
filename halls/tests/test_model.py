0189fb3154e8fac0fc7b264bfac72bc1e5be9bbd
和田翔大 <wkake0568@gmail.com>
2021-08-25 13:15:43 +0900

    add test for Video model

--- halls/tests/test_model.py
+++ halls/tests/test_model.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class HallModelTests(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username='jacob', email='jacob@…', password='top_secret')
+        self.user = User.objects.create_user(username='taro', email='taro@…', password='top_secret')
 
     def test_is_empty(self):
         saved_halls = Hall.objects.all()
@@ -16,7 +16,7 @@ class HallModelTests(TestCase):
         saved_halls = Hall.objects.all()
         self.assertEqual(saved_halls.count(), 1)
 
-    def test_saving_and_retrieving_post(self):
+    def test_saving_and_retrieving_hall(self):
         hall = Hall()
         title = 'Tennis'
         hall.title = title
@@ -28,3 +28,38 @@ class HallModelTests(TestCase):
 
         self.assertEqual(actual_hall.title, title)
         self.assertEqual(actual_hall.user, self.user)
+
+
+class VideoModelTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='taro', email='taro@…', password='top_secret')
+        self.hall = Hall(title='Sports', user=self.user)
+        self.hall.save()
+
+    def test_is_empty(self):
+        saved_videos = Video.objects.all()
+        self.assertEqual(saved_videos.count(), 0)
+
+    def test_is_one(self):
+        title = 'test title'
+        url = 'https://www.youtube.com/watch?v=K8hF7qQE5nQ&list=LLRdrrwPIQqhSvZ7F-W2A_rg&index=5&t=0s'
+        youtube_id = 'K8hF7qQE5nQ'
+        video = Video(title='test title', url='https://www.youtube.com/watch?v=K8hF7qQE5nQ&list=LLRdrrwPIQqhSvZ7F-W2A_rg&index=5&t=0s', youtube_id='K8hF7qQE5nQ', hall=self.hall)
+        video.save()
+        saved_videos = Video.objects.all()
+        self.assertEqual(saved_videos.count(), 1)
+
+    def test_saving_and_retrieving_video(self):
+        title = 'test title'
+        url = 'https://www.youtube.com/watch?v=K8hF7qQE5nQ&list=LLRdrrwPIQqhSvZ7F-W2A_rg&index=5&t=0s'
+        youtube_id = 'K8hF7qQE5nQ'
+        video = Video(title='test title', url='https://www.youtube.com/watch?v=K8hF7qQE5nQ&list=LLRdrrwPIQqhSvZ7F-W2A_rg&index=5&t=0s', youtube_id='K8hF7qQE5nQ', hall=self.hall)
+        video.save()
+
+        saved_videos = Video.objects.all()
+        actual_video = saved_videos[0]
+
+        self.assertEqual(actual_video.title, title)
+        self.assertEqual(actual_video.url, url)
+        self.assertEqual(actual_video.youtube_id, youtube_id)
+        self.assertEqual(actual_video.hall, self.hall)
